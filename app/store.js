d78e7ba483a5465364fe485e854b696cdcff1be0
Taylor Thompson <ttaythompson@gmail.com>
2019-05-11 21:36:41 -0400

    added a thunk middleware

--- app/store.js
+++ app/store.js
@@ -1,9 +1,20 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import axios from "axios";
-import rootReducer from "./reducers";
+import rootReducer, { displayCandies } from "./reducers";
 import loggingMiddleware from "redux-logger"; // https://github.com/evgenyrodionov/redux-logger
 import thunkMiddleware from "redux-thunk"; // https://github.com/gaearon/redux-thunk
+
+export const getCandiesThunk = () => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get("/api/candies");
+      dispatch(displayCandies(data));
+    } catch (err) {
+      console.log("Uh-oh there was an issue loading your candies", err);
+    }
+  };
+};
 
 export default createStore(
   rootReducer,
